5c7a06dfdd025f28be3931bb1a5a412ff2547daf
Tokiarivelo <anjaniaina.toky14@gmail.com>
2025-05-10 13:36:00 +0300

    initial commit

---
+++ backend/graphql/src/auth/login/register.input.ts
@@ -0,0 +1,17 @@
+import { InputType, Field } from '@nestjs/graphql';
+import { IsString, IsEmail } from 'class-validator';
+
+@InputType()
+export class RegisterInput {
+  @Field()
+  @IsEmail()
+  email!: string;
+
+  @Field()
+  @IsString()
+  password!: string;
+
+  @Field()
+  @IsString()
+  username!: string;
+}
